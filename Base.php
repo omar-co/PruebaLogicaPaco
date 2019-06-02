df648a2c184bca81e60e54d7122827af9e164d28
Omar Coronado <omar.goco@gmail.com>
2019-06-02 15:45:17 -0500

    Initial commit

---
+++ Base.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Omar
+ * Date: 02/06/2019
+ * Time: 15:37
+ */
+
+class Base
+{
+
+    public static function array1()
+    {
+        return [
+            '5698' => [
+                'id' => 5698,
+                'quantity' => 5
+            ],'6708' => [
+                'id' => 6708,
+                'quantity' => 78
+            ],'9043' => [
+                'id' => 9043,
+                'quantity' => 98
+            ],'9048' => [
+                'id' => 9048,
+                'quantity' => 67
+            ],'80495' => [
+                'id' => 80495,
+                'quantity' => 55
+            ],'60493' => [
+                'id' => 60493,
+                'quantity' => 50
+            ],
+        ];
+    }
+    public static function array2()
+    {
+        return [
+            '80594' => [
+                'id' => 80594,
+                'quantity' => 89
+            ],'50934' => [
+                'id' => 50934,
+                'quantity' => 78
+            ],'80471' => [
+                'id' => 80471,
+                'quantity' => 98
+            ],'9048' => [
+                'id' => 9048,
+                'quantity' => 67
+            ],'80495' => [
+                'id' => 80495,
+                'quantity' => 56
+            ],'60493' => [
+                'id' => 60493,
+                'quantity' => 50
+            ],
+        ];
+    }
+}
